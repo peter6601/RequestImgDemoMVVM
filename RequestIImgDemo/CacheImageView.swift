405d4509970d1e2175885ff82795a333fa047975
丁暐哲 <peter779701@gmail.com>
2018-04-03 10:14:36 +0800

    [fix] fix useless code

--- RequestIImgDemo/CacheImageView.swift
+++ RequestIImgDemo/CacheImageView.swift
@@ -49,57 +49,26 @@ extension CacheImageView {
         } else {
             image = configuration.placeholderImage
             
-//            var taskOne = DownloadService.shared.download(request: URLRequest(url: url))
-//            taskOne.completionHandler = { [weak self] in
-//                switch $0 {
-//                case .failure(let error):
-//                    print(error)
-//                case .success(let data):
-//                    print("Number of bytes: \(data.count)")
-//                    guard let image = UIImage(data: data) else { return }
-//                    DispatchQueue.main.async { [weak self] in
-//                        self?.image = image
-//                    }
-//                }
-//            }
-//            taskOne.progressHandler = { [weak self] in
-//                print("Task2: \($0)")
-//            }
-//            taskOne.resume()
             
-                        currentTask = URLSession.shared.dataTask(with: url) { (data, response, error) in
-                            guard let img = data else {
-                                print("fail \(url)")
-                                return
-                            }
-            
-                            guard let responseURL = response?.url else { return }
-                            guard let orginURL = self.originRequsetURL, orginURL == responseURL else { return }
-                            print("download \(responseURL.absoluteString)")
-                            guard let image = UIImage(data: img) else { return }
-                            DispatchQueue.main.async { [weak self] in
-                                self?.image = image
-                            }
-                            self.responseURL = responseURL
-                            self.cacheImage.setObject(image, forKey: url as NSURL)
-                        }
+            currentTask = URLSession.shared.dataTask(with: url) { (data, response, error) in
+                guard let img = data else {
+                    print("fail \(url)")
+                    return
+                }
+                
+                guard let responseURL = response?.url else { return }
+                guard let orginURL = self.originRequsetURL, orginURL == responseURL else { return }
+                print("download \(responseURL.absoluteString)")
+                guard let image = UIImage(data: img) else { return }
+                DispatchQueue.main.async { [weak self] in
+                    self?.image = image
+                }
+                self.responseURL = responseURL
+                self.cacheImage.setObject(image, forKey: url as NSURL)
+            }
         }
     }
 }
 
 
-//extension CacheImageView: URLSessionDelegate, URLSessionDataDelegate {
-//    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive response: URLResponse, completionHandler: @escaping (URLSession.ResponseDisposition) -> Swift.Void) {
-//       progressLentgh =  Int(response.expectedContentLength)
-//        completionHandler(URLSession.ResponseDisposition.allow)
-//
-//    }
-//
-//    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
-//        buffer.append(data)
-//        let percentageDownloaded = Float(buffer.length) / Float(progressLentgh)
-//
-//    }
-//
-//}
 
